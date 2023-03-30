df2458b0a06d031b0c93fa60add58fa8f2ad9fbe
Klayvert <klayvert.ryan16@gmail.com>
2023-03-30 01:51:21 -0300

    Ajustando tratativas de data para identificar os feriados em questao

--- feriados/core/views.py
+++ feriados/core/views.py
@@ -5,7 +5,7 @@ dia_hoje = date.today().strftime('%d/%m')
 dia_tiradentes = date(day=21, month=4, year=date.today().year)
 dia_natal = date(day=25, month=12, year=date.today().year)
 # dia_hoje = '25/12'
-# dia_hoje = '21/04'
+# dia_hoje = '21/04' 
 
 def natal(request):
     contexto = {
