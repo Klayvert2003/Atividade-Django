e9852835bca2c093ee5464cacdf5b9bbcb98b076
Klayvert <klayvert.ryan16@gmail.com>
2023-03-30 01:01:38 -0300

    Ajusta projeto

--- feriados/core/urls.py
+++ feriados/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.natal),
-    path('tiradentes', views.tiradentes),
+    path('',views.natal),
+    path('tiradentes/', views.redireciona, name='tiradentes'),
+    path('',views.tiradentes),
 ]
--- feriados/core/views.py
+++ feriados/core/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
-from datetime import datetime
+from django.shortcuts import render, redirect
+from datetime import date
+
+dia_hoje = date.today().strftime('%d/%m')
+
+dia_hoje = '21/04'
 
 def natal(request):
-    contexto = {"natal": True, "carnaval": False}
+    contexto = {
+        'natal': True if dia_hoje == '25/12' else False
+    }
     return render(request, 'natal.html', contexto)
 
 def tiradentes(request):
-    contexto = {"tiradentes": True, "outroferiado": False}
+    contexto = {
+        'tiradentes': True if dia_hoje == '21/04' else False
+    }
     return render(request, 'tiradentes.html', contexto)
+
+def redireciona(request):
+    return render(request, 'tiradentes.html')
